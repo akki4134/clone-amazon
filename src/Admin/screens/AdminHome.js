0ce1c433a798eddef8560ab1e0653820f15ac437
akki4134 <yellagandulaakhil@gmail.com>
2021-06-06 22:25:04 +0530

    admin side bar

--- src/Admin/screens/AdminHome.js
+++ src/Admin/screens/AdminHome.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import './AdminHome.css'
+
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { SidebarData } from '../container/SidebarData';
+
+
+import { IconContext } from 'react-icons';
 
 const AdminHome = () => {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
-        <div>
-            Dashboard
-        </div>
-    )
+        <>
+            {/* <div className="container"> */}
+                <IconContext.Provider value={{ color: '#fff' }}>
+                    <div className='navbar'>
+                        <Link to='#' className='menu-bars'>
+                            <FaIcons.FaBars onClick={showSidebar} />
+                        </Link>
+                    </div>
+                    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                        <ul className='nav-menu-items' onClick={showSidebar}>
+                            {SidebarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </nav>
+                </IconContext.Provider>
+            {/* </div> */}
+        </>
+    );
 }
 
-export default AdminHome
+export default AdminHome;
